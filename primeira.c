283cd81744a5aba4be693094c3e934e1fce2b8e2
Matheus Abreu <107859047+oclava@users.noreply.github.com>
2023-11-07 13:52:19 -0300

    Update primeira.c

--- primeira.c
+++ primeira.c
@@ -7,7 +7,12 @@ int main (){
     for (int i = 0; i < 10; i++)
     {
         scanf("%d", &numero[i]);
-        printf("%d ", numero[i]);
+        printf("%d", numero[i]);
+        if (i<9)
+        {
+            printf(" ");
+        }
+        
     }
     printf("\n");
 
@@ -17,7 +22,14 @@ int main (){
         for (int i = 0; i < z; i++)
         {
             numero[i] = numero[i] + numero[i+1];
-            printf("%d ", numero[i]);
+            
+            printf("%d", numero[i]);
+
+            if (i < z - 1)
+            {
+                printf(" ");
+            }
+            
         }
         printf("\n");
     }
